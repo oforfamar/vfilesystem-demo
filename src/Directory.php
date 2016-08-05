ea4a1d3e910f9f79138b807e07d7e4ef74a95d7e
Tudor Roman <tudor.roman@code932.com>
2016-08-05 07:47:39 +0000

    adds vagrant file and vagrant install script; adds Directory properties and getters for properties

--- src/Directory.php
+++ src/Directory.php
@@ -9,10 +9,15 @@ class Directory
     private $mode;
     private $recursive;
 
+    private $parent;
+    private $permissions;
+    private $createdDate;
+    private $updatedDate;
+
     /**
      * @param string $name
-     * @param int $mode
-     * @param bool $recursive
+     * @param int    $mode
+     * @param bool   $recursive
      */
     public function __construct($name, int $mode, bool $recursive)
     {
@@ -29,4 +34,35 @@ class Directory
         return $this->name;
     }
 
+    /**
+     * @return mixed
+     */
+    public function getParent()
+    {
+        return $this->parent;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPermissions()
+    {
+        return $this->permissions;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedDate()
+    {
+        return $this->createdDate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUpdatedDate()
+    {
+        return $this->updatedDate;
+    }
 }
